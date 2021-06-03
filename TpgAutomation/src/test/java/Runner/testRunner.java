f5d8486e2d21f32ace1ac0741fa77f7a36a66de9
SYSTEMS\awais.sultan <042waisi@gmail.com>
2021-06-03 19:01:53 +0500

    Credentials Changes

--- TpgAutomation/src/test/java/Runner/testRunner.java
+++ TpgAutomation/src/test/java/Runner/testRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 features = "src/test/resources/Features"
 ,glue= {"TpgStepDefinitions"}
-,tags= {"@selectPGWandEnterDetails"}
+,tags= {"@MARefundFunctionality"}
 )
 public class testRunner {
 
